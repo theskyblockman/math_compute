2d9c45241da19913ab8ff6ab8aacba00d49c5e4e
theskyblockman <theskyblockman@gmail.com>
2024-08-25 15:14:12 +0200

    Set the correct basic example

--- example/math_compute_example.dart
+++ example/math_compute_example.dart
@@ -2,7 +2,7 @@ import 'package:math_compute/base.dart';
 
 void main() {
   // Example coming from https://en.wikipedia.org/wiki/Shunting_yard_algorithm#Detailed_examples
-  final input = '5% + 100'; // 3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2 ^ 3
+  final input = '3 + 4 × 2 ÷ ( 1 − 5 ) ^ 2 ^ 3';
   print('Computing "$input"...');
   final result = compute(input);
 
